3f610c5dffd571369b50601629fd87c3e16fc7ac
Kimberly Brunette <0834238@etu.cegepjonquiere.ca>
2025-05-02 08:27:30 -0400

    ajout base page

---
+++ app/boutique/panier/page.js
@@ -0,0 +1,10 @@
+//administration\ajoutmusique\page.js
+import Header from "../../components/Header";
+
+export default async function PagePanier() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
