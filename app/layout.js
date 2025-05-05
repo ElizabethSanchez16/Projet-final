6fb700a5a302c95cf609ca68d3dd4b421b846927
Kimberly Rousse <kimberly.rousse@hotmail.com>
2025-05-05 17:34:00 -0400

    Voir journal

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+          crossOrigin="anonymous"
+        ></script>
+        </body>
     </html>
   );
 }
