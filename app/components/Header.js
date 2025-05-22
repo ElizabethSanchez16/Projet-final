397581b56dc94cde568ae182eee737def7c1a989
Kimberly Brunette <0834238@etu.cegepjonquiere.ca>
2025-05-22 09:51:49 -0400

    Ajout plan de présentation et header

--- app/components/Header.js
+++ app/components/Header.js
@@ -96,6 +96,12 @@ export default function Header() {
             </li>
           </ul>
 
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="">Diagramme UML</a>
+            </li>
+          </ul>
+
           <form className="d-flex me-2" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="checkout" type="submit">Search</button>
